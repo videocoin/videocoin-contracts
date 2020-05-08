5a6ad6b5a2560dc29518a92088b4d10a9715840d
tarassh <tshchybo@gmail.com>
2020-05-08 16:28:19 +0300

    fix nonce too low issue for deployment

--- migrations/3_deploy_staking_manager.js
+++ migrations/3_deploy_staking_manager.js
@@ -10,7 +10,7 @@ const unbondingPeriod = 10;
 const slashRate = 50;
 const slashFund = "0x0000000000000000000000000000000000000000";
 
-module.exports = function (deployer, network, accounts) {
+module.exports = async function (deployer, network, accounts) {
   var from;
   if (network === 'everest') {
     // Key order is defined in everest provider
@@ -21,7 +21,7 @@ module.exports = function (deployer, network, accounts) {
 
   console.log(`Deploying ${StakingManager.contractName} from ${from} on network: ${network}`);
 
-  deployer.deploy(
+  await deployer.deploy(
     StakingManager,
     minDelegation,
     minSelfDelegation,
