f558a85b7fc3b10e85303afb0a7c6f9a93988579
tarassh <tshchybo@gmail.com>
2020-05-12 21:09:30 +0300

    add everest staging values

--- migrations/3_deploy_staking_manager.js
+++ migrations/3_deploy_staking_manager.js
@@ -1,25 +1,42 @@
-var StakingManager = artifacts.require("StakingManager");
-
-const sixvids = web3.utils.toWei("6");
-const tenvids = web3.utils.toWei("10");
-
-const minDelegation = sixvids;
-const minSelfDelegation = tenvids;
-const approvalPeriod = 5;
-const unbondingPeriod = 10;
-const slashRate = 50;
-const slashFund = "0x0000000000000000000000000000000000000000";
+const StakingManager = artifacts.require("StakingManager");
 
 module.exports = async function (deployer, network, accounts) {
+  const twovids = web3.utils.toWei("2");
+  const sixvids = web3.utils.toWei("6");
+  const tenvids = web3.utils.toWei("10");
+
   var from;
+  var minDelegation, minSelfDelegation;
+  var approvalPeriod, unbondingPeriod;
+  var slashRate, slashFund;
   if (network === 'everest') {
     // Key order is defined in everest provider
     from = accounts[1];
+
+    minDelegation = twovids;
+    minSelfDelegation = tenvids;
+    approvalPeriod = 100;
+    unbondingPeriod = 100;
+    slashRate = 0;
+    slashFund = "0x0000000000000000000000000000000000000000";
   } else {
     from = accounts[0];
+
+    minDelegation = sixvids;
+    minSelfDelegation = tenvids;
+    approvalPeriod = 5;
+    unbondingPeriod = 10;
+    slashRate = 50;
+    slashFund = "0x0000000000000000000000000000000000000000";
   }
 
   console.log(`Deploying ${StakingManager.contractName} from ${from} on network: ${network}`);
+  console.log(`required stake:      ${minSelfDelegation}`);
+  console.log(`required delegation: ${minDelegation}`);
+  console.log(`approval period:     ${approvalPeriod}`);
+  console.log(`unbonding period:    ${unbondingPeriod}`);
+  console.log(`slash rate:          ${slashRate}`);
+  console.log(`slash fund account:  ${slashFund}`);
 
   await deployer.deploy(
     StakingManager,
