f820db95f75262259db6793314fdfa6d764821b1
dmitry <yashulyak@gmail.com>
2020-06-11 10:38:21 +0300

    Skip escrow deployment on everest

--- migrations/6_deploy_staking_escrow.js
+++ migrations/6_deploy_staking_escrow.js
@@ -1,6 +1,9 @@
 const StakingEscrow = artifacts.require("StakingEscrow");
 
 module.exports = async function (deployer, network, accounts) {
+  if (network === "everest") {
+    console.log("Skip ${StakingEscrow.contractName} deployment");
+  }
   const erc20 = process.env.ESCROW_ERC20_ADDRESS;
   const from = accounts[0];
   console.log(`Deploying ${StakingEscrow.contractName}. ERC20 ${erc20}. Owner ${from} on network: ${network}`);
