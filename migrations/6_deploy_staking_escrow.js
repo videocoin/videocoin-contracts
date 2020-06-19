a11c89d618c41e93f8379decd3e7f47f7d5ebc7a
dmitry <yashulyak@gmail.com>
2020-06-19 08:07:17 +0300

    Deploy TestERC on development network

--- migrations/6_deploy_staking_escrow.js
+++ migrations/6_deploy_staking_escrow.js
@@ -1,11 +1,18 @@
 const StakingEscrow = artifacts.require("StakingEscrow");
+const TestERC = artifacts.require("TestERC");
 
 module.exports = async function (deployer, network, accounts) {
   if (network === "everest") {
     console.log("Skip ${StakingEscrow.contractName} deployment");
   }
-  const erc20 = process.env.ESCROW_ERC20_ADDRESS;
+
   const from = accounts[0];
+  var erc20 = process.env.ESCROW_ERC20_ADDRESS;
+  if (network === "development") {
+     await deployer.deploy(TestERC, { from });
+     erc20 = TestERC.address;
+  }
+
   console.log(`Deploying ${StakingEscrow.contractName}. ERC20 ${erc20}. Owner ${from} on network: ${network}`);
   await deployer.deploy(StakingEscrow, erc20, { from });
   console.log("Done");
